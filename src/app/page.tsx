2479d925fa2f41b7056b8de61b59a6d59daa43d5
Sayan Singha Chowdhury <sayansinghachowdhury.dev@gmail.com>
2026-05-09 21:34:49 +0530

    add ui component Shadcn add button, card, pophover modify ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,33 @@
+import BasicAge from "@/components/BasicAge";
+import {
+	Card,
+	CardContent,
+	CardHeader,
+	CardTitle,
+} from "@/components/shadcnui/card";
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
 	title: "AgeTracker|Basic Age Calculator",
 	description: "Age Calculator with Basic",
 };
-
+// Basic Page
 const page = () => {
-	return <section className="grid h-[90dvh] place-items-center"></section>;
+	return (
+		<section className="grid h-[90dvh] place-items-center">
+			<Card className="w-sm border border-white/20 bg-white/10 shadow-lg backdrop-blur-lg">
+				<CardHeader>
+					<CardTitle className="text-center text-2xl font-semibold">
+						Basic Age Calculator
+					</CardTitle>
+				</CardHeader>
+				<CardContent>
+					{/*Basic age Component call */}
+					<BasicAge />
+				</CardContent>
+			</Card>
+		</section>
+	);
 };
 
 export default page;
---
+++ src/components/BasicAge.tsx
@@ -0,0 +1,7 @@
+"use client";
+
+const BasicAge = () => {
+	return <div>Enter</div>;
+};
+
+export default BasicAge;
